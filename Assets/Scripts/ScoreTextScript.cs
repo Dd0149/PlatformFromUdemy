8d36f62daef5c7aaeffc8db8dbc39e38455bb216
Jason Dedrick <maskryashkaproductions@gmx.com>
2022-05-20 14:23:27 -0500

    ChangedScriptFile

--- Assets/Scripts/ScoreTextScript.cs
+++ Assets/Scripts/ScoreTextScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public class ScoreTextSCript : MonoBehaviour
+public class ScoreTextScript : MonoBehaviour
 {
     public TMP_Text text;
     public int coinAmount;
