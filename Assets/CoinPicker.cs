b7ea3ccee72b112720459a84177cb121fe6214e7
Jason Dedrick <maskryashkaproductions@gmx.com>
2022-05-20 13:35:53 -0500

    AddedCoin Collect

---
+++ Assets/CoinPicker.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPicker : MonoBehaviour
+{
+    private float coins =0;
+     private void OnTriggerEnter2D(Collider2D other) {
+        if(other.transform.tag == "JellyCoin"){
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreTextSCript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ScoreTextSCript : MonoBehaviour
+{
+    public TMP_Text text;
+    public int coinAmount;
+       void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = coinAmount.ToString();
+    }
+}
