4615adec5a2e912cd217510b44f6873ba6e6bbc8
Coben <j.j.viser1@gmail.com>
2022-05-21 17:11:16 -0500

    Tried Adding Coin Counter Script

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinCounter : MonoBehaviour
+{
+    public TMP_Text text;
+    public float count;
+    public float currency;
+    public GameObject player;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+          
+        
+    //}
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{ 
+    //    if (player.tag == "Player")
+    //    {
+    //        count += currency;
+    //        text.text = count.ToString() + "Coins";
+    //    }
+    //}
+}
